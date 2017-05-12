6f2f11cdc80f77aa83d47689f3c3cab1730adbe3
daiscns <dais.cns@gmail.com>
2017-05-12 20:17:18 +0900

    got limits implemented

--- sensitiveSql.kt
+++ sensitiveSql.kt
@@ -12,24 +12,28 @@ import java.util.LinkedHashMap
 import org.yaml.snakeyaml.Yaml;
 
 val Lines_conf = """# configuration file
-#   [...]: default value
-#   'y': yes or on(switched), 'n': no or off
+  #   [...]: default value
+  #   'y': yes or on(switched), 'n': no or off
 
-'Class_forName': 'oracle.jdbc.driver.OracleDriver'
-  # for oracle: 'oracle.jdbc.driver.OracleDriver'
+'Class_forName': 'org.sqlite.JDBC'
   # for sqlite: 'org.sqlite.JDBC'
+  # for oracle: 'oracle.jdbc.driver.OracleDriver'
 'uri_DriverManager': 'jdbc:sqlite:test.db'
-  # for oracle:
-    # 'jdbc:oracle:thin:@111.222.333.444:1521:'
-    # 'u/p@ip/s'
   # for sqlite: 'jdbc:sqlite:test.db'
+  # for oracle:
+  #   'jdbc:oracle:thin:@111.222.333.444:1521:sid'
+  #   'u/p@ip/sid'
+  #     to see 'sid': SQL> select sys_context('USERENV','INSTANCE_NAME') from dual;
 'dba_user': 'ora'
 'dba_pw': 'cle'
 'sql':
-  - select count(*) from muser;
+  - select count(*) from muser
   - |
     select * from muser
-      where name!='yy';
+      where name!='yy'
+  # do NOT put ';' semi-column as the end of sql: oracle can NOT read it...
+
+'query_timeout': 5
 'safety_counts': 100000
 'safety_seconds': 6
 'file_name_out': 'out.txt'
@@ -69,19 +73,27 @@ fun do_dba() {
 	val sqls: List<String> = params.get("sql") as List<String>
 //		println( sqls[1] )		println( params.get("Class_forName") )
 	val class_forName = params.get("Class_forName") as String
+	val query_timeout = (params.get("query_timeout") as Int?)?: 30
+	val safety_counts = (params.get("safety_counts") as Int?)?: 100000
+	val safety_seconds = (params.get("safety_seconds") as Int?)?: 20
+	val file_name_out 		= params.get("file_name_out") as String
+	val uri_DriverManager 	= params.get("uri_DriverManager") as String
+	val dba_user 				= params.get("dba_user") as String
+	val dba_pw 					= params.get("dba_pw") as String
+	var flg_overtime			= false;
+
 	Class.forName(class_forName)
 	try {
-		val conn = if(class_forName=="org.sqlite.JDBC") DriverManager.getConnection(
-				params.get("uri_DriverManager") as String ) else DriverManager.getConnection(
-				params.get("uri_DriverManager") as String,
-				params.get("dba_user") as String,
-				params.get("dba_pw") as String )
+		val conn = if(class_forName=="org.sqlite.JDBC"
+							) DriverManager.getConnection(uri_DriverManager)
+						else DriverManager.getConnection(uri_DriverManager, dba_user, dba_pw )
 		val sttm = conn.createStatement()
 		for( sql in sqls ){
-			sttm.setQueryTimeout(30)
+			sttm.setQueryTimeout(query_timeout)
 			var rs: ResultSet = sttm.executeQuery(sql)
 			var rsmd: ResultSetMetaData = rs.getMetaData()
 			var cnt: Int = 0
+			val msec_started = (System.currentTimeMillis() as Long)?: 0L
 			while (rs.next()) {
 				for (i in IntRange(1, rsmd.getColumnCount())) {
 					print(rs.getString(i))
@@ -89,9 +101,22 @@ fun do_dba() {
 				}
 				print(System.getProperty("line.separator"))
 				cnt++
+				if(safety_counts<cnt) break;
+				println("  safety_sec: ${safety_seconds}, during_msec: ${System.currentTimeMillis() - msec_started}")
+				if(safety_seconds*1000L < System.currentTimeMillis() - msec_started){
+					flg_overtime = true
+					break
+				}
 			}
-
 			println(" rs size: ${cnt}")
+			if(safety_counts<cnt){
+				println("[warn] query records has been over the limit: ${cnt}")
+				break;
+			}
+			if(flg_overtime){
+				println("[warn] query time has been over the limit: ${safety_seconds} seconds")
+				break;
+			}
 		}
 		sttm.close()
 		conn.close()
---
+++ win_invoker.bat
@@ -0,0 +1,18 @@
+@echo on
+umask 022
+
+set Bin_java=c:\tools\pleiades\java\8\bin\java
+set Option_java=-Xmx2048m -Xms384m
+set CLASSPATH=.;lib\*;%CLASSPATH%
+set Option_cp=-classpath koracler.jar;lib\*
+set Path_work=c:\works\koracler-master
+
+rem cd %Path_work%
+rem kotlinc sensitiveSql.kt -include-runtime -d koracler.jar -cp $CLASSPATH
+
+%Bin_java% %Option_java% %Option_cp% SensitiveSqlKt
+
+rem java -Xmx2048m -Xms384m -classpath koracler.jar:lib/* SensitiveSqlKt
+
+	rem in case on Windows with pure console:
+		rem c:\tools\pleiades\java\8\bin\java -Xmx2048m -Xms384m -classpath koracler.jar;lib\* SensitiveSqlKt
